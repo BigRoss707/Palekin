33e9f2d3367e4f739149dcbc2ce77320ac819077
Jacob Kramer <jpnkramer@gmail.com>
2018-06-22 18:13:48 -0600

    Playercontroller and Base PhysicsObject Added

---
+++ Unity/Palekin/Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+
+    public float minGroundNormalY = .65f;
+    public float gravityModifier = 1f;
+    public float gravity = 5f;
+
+    private const float minMoveDistance = 0.001f;
+    private float maxVertMove;
+    private const float shellRadius = 0.01f;
+
+    private Rigidbody2D rigidBody;
+    protected Vector2 velocity;
+    protected Vector2 targetVelocity;
+    private Vector2 groundNormal;
+    protected bool grounded;
+    private ContactFilter2D contactFilter; 
+
+    void OnEnable()
+    {
+        rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Start()
+    {
+        maxVertMove = gravity * gravityModifier * Physics2D.gravity.magnitude;
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+    protected virtual void Update()
+    {
+        targetVelocity = Vector2.zero;
+        ComputeVelocity();
+    }
+
+    protected virtual void ComputeVelocity()
+    {
+
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        grounded = false;
+        ApplyGravity();
+        ApplyVelocity();
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 moveX = deltaPosition.x * new Vector2(groundNormal.y, -groundNormal.x);
+        Vector2 move = moveX;
+        Move(move, false);
+        move = Vector2.up * deltaPosition.y;
+        Move(move, true);
+    }
+
+    void Move(Vector2 move, bool yMove)
+    {
+        float distance = move.magnitude;
+        RaycastHit2D[] hits = new RaycastHit2D[16];
+        List<RaycastHit2D> hitsList = new List<RaycastHit2D>();
+
+        if(distance >  minMoveDistance)
+        {
+            int count = rigidBody.Cast(move, contactFilter, hits, distance + shellRadius);
+            for(int i = 0; i < count; i++)
+            {
+                hitsList.Add(hits[i]);
+            }
+
+            for(int i = 0; i < hitsList.Count; i++)
+            {
+                Vector2 currentNormal = hitsList[i].normal;
+                if(currentNormal.y > minGroundNormalY)
+                {
+                    grounded = true;
+                    if(yMove)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if(projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitsList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+
+        rigidBody.position = rigidBody.position + move.normalized * distance;
+    }
+
+    void ApplyGravity()
+    {
+        velocity += gravity * gravityModifier* Physics2D.gravity * Time.deltaTime;
+        if(-velocity.y > maxVertMove)
+        {
+            velocity.y = -maxVertMove;
+        }
+    }
+    
+    void ApplyVelocity()
+    {
+        velocity.x = targetVelocity.x;
+    }
+
+}
---
+++ Unity/Palekin/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject {
+
+    public struct Inputs
+    {
+        //Axis
+        public float VertAxis;
+        public float HoriAxis;
+
+        //Jump
+        public bool Jump;
+        public bool JumpUp;
+        public bool JumpDown;
+
+        //Keys
+        public bool W;
+        public bool W_Down;
+        public bool W_Up;
+        public bool A;
+        public bool A_Down;
+        public bool A_Up;
+        public bool S;
+        public bool S_Down;
+        public bool S_Up;
+        public bool D;
+        public bool D_Down;
+        public bool D_Up;
+    }
+
+    #region PublicVariables
+    public bool haltInput = false;
+    public float maxSpeed = 30;
+    public float speed = 18;
+    public float speedScalar = 1;
+    public float jumpSpeed = 8;
+    public float jumpSpeedScalar = 1;
+    #endregion
+
+    #region PrivateVariables
+    private Inputs input;
+    private SpriteRenderer spriteRenderer;
+
+    //private Animator animator;
+    #endregion
+
+
+    void Awake()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        //animator = this.GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        //Initialization of public members
+        haltInput = false;
+
+        //Initialization of private members
+        input = new Inputs();
+    }
+
+    protected override void FixedUpdate()
+    {
+        TakeInput(haltInput);
+        base.FixedUpdate();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        base.ComputeVelocity();
+        Vector2 move = Vector2.zero;
+        move.x = input.HoriAxis;
+
+        if(input.JumpDown && grounded)
+        {
+            velocity.y = jumpSpeed * jumpSpeedScalar;
+        }
+        else if(input.JumpUp)
+        {
+            if(velocity.y  > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x >  0.01f) : (move.x < 0.01f));
+        if(flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        //animator.SetBool("grounded", grounded);
+        //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+        targetVelocity = move * speed;
+        //Debug.Log("Target Velocity: " + targetVelocity.ToString());
+        Debug.Log("Velocity: " + velocity.ToString());
+    }
+
+    void TakeInput(bool haltInput)
+    {
+        if(!haltInput)
+        {
+            input.VertAxis = Input.GetAxis("Vertical");
+            input.HoriAxis = Input.GetAxis("Horizontal");
+
+            input.Jump = Input.GetKey(KeyCode.Space);
+            input.JumpUp = Input.GetKeyUp(KeyCode.Space);
+            input.JumpDown = Input.GetKeyDown(KeyCode.Space);
+
+            input.W = Input.GetKey(KeyCode.W);
+            input.W_Down = Input.GetKeyDown(KeyCode.W);
+            input.W_Up = Input.GetKeyUp(KeyCode.W);
+
+            input.A = Input.GetKey(KeyCode.A);
+            input.A_Down = Input.GetKeyDown(KeyCode.A);
+            input.A_Up = Input.GetKeyUp(KeyCode.A);
+
+            input.S = Input.GetKey(KeyCode.S);
+            input.S_Down = Input.GetKeyDown(KeyCode.S);
+            input.S_Up = Input.GetKeyUp(KeyCode.S);
+
+            input.D = Input.GetKey(KeyCode.D);
+            input.D_Down = Input.GetKeyDown(KeyCode.D);
+            input.D_Up = Input.GetKeyUp(KeyCode.D);
+
+            
+        }
+        
+    }
+
+}
